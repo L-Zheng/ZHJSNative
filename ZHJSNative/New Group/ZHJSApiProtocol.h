38f1d8476a25bd26615c33905698ea3912689939
L-Zheng <418736259@qq.com>
2020-07-08 10:03:50 +0800

    update js交互文档

--- ZHJSNative/New Group/ZHJSApiProtocol.h
+++ ZHJSNative/New Group/ZHJSApiProtocol.h
@@ -25,28 +25,47 @@ typedef void(^ZHJSApiAliveBlock)(id result, NSError *error, BOOL alive);
 //ios api方法名前缀 如：js_
 - (NSString *)zh_iosApiPrefixName;
 
+
 /**
  ⚠️⚠️⚠️添加API步骤：
  在服从协议ZHJSApiProtocol的类中实现方法即可：
-     异步方法
-       - (void)js_<#functionName#><##>:(NSDictionary *)params{}
-       - (void)js_<#functionName#><##>:(NSDictionary *)params callBack:(ZHJSApiBlock)callBack{}
- 
-     同步方法
-       //返回JS类型Object
-       - (NSDictionary *)js_<#functionName#><##>Sync:(NSDictionary *)params{}
- 
-       //返回JS类型Array
-       - (NSArray *)js_<#functionName#><##>Sync:(NSDictionary *)params{}
- 
-       //返回JS类型String
-       - (NSString *)js_<#functionName#><##>Sync:(NSDictionary *)params{}
- 
-       //返回JS类型Number
-       - (NSNumber *)js_<#functionName#><##>Sync:(NSDictionary *)params{}
-       
-       //返回JS类型Boolean：@(YES)、@(NO)
-       - (NSNumber *)js_<#functionName#><##>Sync:(NSDictionary *)params{}
+ id参数：类型可能为
+        NSDictionary
+        NSString
+        NSNumber：(js中的number 或者 boolean类型传到原生都为NSNumber类型)
+        NSArray
+        nil
+ callBack参数：
+        ZHJSApiBlock：只能调用一次，再次调用无效
+        ZHJSApiAliveBlock：
+            alive：YES 可调用多次
+            alive：NO 只能调用一次，再次调用无效
+ 异步方法
+   - (void)js_<#functionName#><##>{}
+   - (void)js_<#functionName#><##>:(id)params{}
+   - (void)js_<#functionName#><##>:(id)params callBack:(ZHJSApiBlock)callBack{}
+   - (void)js_<#functionName#><##>:(id)params callBack:(ZHJSApiAliveBlock)callBack{}
+
+ 同步方法
+   //返回JS类型Object
+   - (NSDictionary *)js_<#functionName#><##>Sync{}
+   - (NSDictionary *)js_<#functionName#><##>Sync:(id)params{}
+
+   //返回JS类型Array
+   - (NSArray *)js_<#functionName#><##>Sync{}
+   - (NSArray *)js_<#functionName#><##>Sync:(id)params{}
+
+   //返回JS类型String
+   - (NSString *)js_<#functionName#><##>Sync{}
+   - (NSString *)js_<#functionName#><##>Sync:(id)params{}
+
+   //返回JS类型Number
+   - (NSNumber *)js_<#functionName#><##>Sync{}
+   - (NSNumber *)js_<#functionName#><##>Sync:(id)params{}
+   
+   //返回JS类型Boolean：@(YES)、@(NO)
+   - (NSNumber *)js_<#functionName#><##>Sync{}
+   - (NSNumber *)js_<#functionName#><##>Sync:(id)params{}
  */
 @end
 
--- template/src/App.vue
+++ template/src/App.vue
@@ -3,7 +3,7 @@
     <PullRefresh @refresh="onRefresh">
       <div @click="clickTest" 
       style="width:100px;height:100px;background-color:orange;">
-      点我
+      点我测试
       </div>
       <div
       style="width:100px;height:100px;background-color:orange;">
@@ -70,6 +70,23 @@ var vm = {
   mounted() {},
   methods: {
     clickTest() {
+      fund.request({
+            url: 'https://dataapineice.1234567.com.cn/community/show/article?serverversion=6.2.5&userid=43fe14e102644bd5b0edcd8fc3306e80&product=EFund&passportid=1010285265217684&deviceid=F0DD802F-6164-439E-9ACB-85763AAE813F&plat=Iphone&ids=20191128154557877210040_300&ctoken=afqqc6c6afacj-q6f1qk-8kjrnej-d1-&utoken=ndecnj-fjne1krck8816q68fkcnfkcnu&version=6.3.0&gtoken=85D646FD659449F7A3E646530A51F372',
+            method: 'GET',
+            success: function (res) {
+                console.log('test-reqyessfdwd')
+                console.log(Object.prototype.toString.call(res))
+                console.log(res)
+                // console.log(Object.prototype.toString.call(res.sd))
+                // console.log(res.sd)
+            },
+            fail: function() {
+            },
+            complete: function() {
+                console.log('completecompletecomplete')
+            }
+        })
+      return;
       window.localStorage.setItem('ffffff', 'xxsdfgd')
       console.log(window.localStorage.getItem('ffffff'))
 
