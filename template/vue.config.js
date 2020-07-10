176d23fe842e9ae774e75dec304b4291063b16bf
L-Zheng <418736259@qq.com>
2020-07-10 10:59:07 +0800

    指定 template 打包路径

--- template/vue.config.js
+++ template/vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   productionSourceMap: false,
+  pages: {
+    index: {
+      entry: './src/main.js',
+      template: './public/index.html',
+    }
+  },
   chainWebpack: config => config.optimization.minimize(false),
   publicPath: './'
 }
