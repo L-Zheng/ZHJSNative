61802c3a9ed790f6ab0c4b75c68175162073b0ce
L-Zheng <418736259@qq.com>
2021-05-23 15:41:16 +0800

    示例首页改为tableview

--- ZHJSNative/ViewController.m
+++ ZHJSNative/ViewController.m
@@ -12,8 +12,9 @@
 #import "ZHDebugPanel.h"
 #import "ZHJSWebTestController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
+@property (nonatomic,strong) UITableView *tableView;
+@property (nonatomic,retain) NSArray *items;
 @end
 
 @implementation ViewController
@@ -21,44 +22,98 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+    self.title = @"示例";
+    self.navigationController.navigationBar.translucent = NO;
+        
+    if (@available(iOS 11.0, *)){
+        self.tableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentAutomatic;
+    }else{
+        self.automaticallyAdjustsScrollViewInsets = YES;
+    }
+    self.tableView.tableFooterView = [UIView new];
+    [self.view addSubview:self.tableView];
     
-    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
-    btn.backgroundColor = [UIColor orangeColor];
-    [btn setTitle:@"调试页" forState:UIControlStateNormal];
-    [btn addTarget:self action:@selector(btnClick) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:btn];
-    
-    UIButton *btn1 = [[UIButton alloc] initWithFrame:CGRectMake(100, CGRectGetMaxY(btn.frame) + 50, 100, 100)];
-    btn1.backgroundColor = [UIColor orangeColor];
-    [btn1 setTitle:@"Web示例" forState:UIControlStateNormal];
-    [btn1 addTarget:self action:@selector(btn1Click) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:btn1];
+    [self loadData];
+    [self.tableView reloadData];
 }
 
-- (void)btnClick{
-    
-//    ZHDebugPanel *panelView = [[ZHDebugPanel alloc] initWithFrame:CGRectMake(0, 300, self.view.bounds.size.width, 300)];
-//    [self.view addSubview:panelView];
-    
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//        [panelView removeFromSuperview];
-//    });
-    
-//    return;
-    
-    
-    
-//    NSURL *url = [NSURL URLWithString:@"https://act.1234567.com.cn/topic/repository/fund-toapp-page/toapp.html?schema=fund://mp.1234567.com.cn/weex/4e11280eef6a4277aa855e98eb385bec/pages/index"];
-//    [[UIApplication sharedApplication] openURL:url options:nil completionHandler:^(BOOL success) {
-//        NSLog(@"--------------------");
-//    }];
-    [self.navigationController pushViewController:[[ZHController alloc] init] animated:YES];
-    
-//    [self.navigationController popToRootViewControllerAnimated:YES];
+- (void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
+    self.tableView.frame = self.view.bounds;
 }
 
-- (void)btn1Click{
-    [self.navigationController pushViewController:[[ZHJSWebTestController alloc] init] animated:YES];
+- (void)loadData{
+    __weak __typeof__(self) weakSelf = self;
+    self.items = @[
+        @{
+            @"title": @"本地html调试",
+            @"block": ^(void){
+                [weakSelf.navigationController pushViewController:[[ZHController alloc] init] animated:YES];
+            }
+        },
+        @{
+            @"title": @"web加载示例",
+            @"block": ^(void){
+                [weakSelf.navigationController pushViewController:[[ZHJSWebTestController alloc] init] animated:YES];
+            }
+        },
+        @{
+            @"title": @"调试控制台",
+            @"block": ^(void){
+                    ZHDebugPanel *panelView = [[ZHDebugPanel alloc] initWithFrame:CGRectMake(0, 300, weakSelf.view.bounds.size.width, 300)];
+                    [weakSelf.view addSubview:panelView];
+                    
+                    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                        [panelView removeFromSuperview];
+                    });
+            }
+        }
+    ];
+}
+
+#pragma mark - UITableViewDelegate
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.items.count;
+}
+
+//- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+//}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *cellID = @"BaseCellId";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
+    }
+    cell.textLabel.numberOfLines = 0;
+    cell.textLabel.adjustsFontSizeToFitWidth = YES;
+    cell.textLabel.text = self.items[indexPath.row][@"title"];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    void (^block) (void) = self.items[indexPath.row][@"block"];
+    if (block) {
+        block();
+    }
+}
+
+#pragma mark - getter
+
+- (UITableView *)tableView {
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+        _tableView.backgroundColor = [UIColor clearColor];
+        
+        _tableView.directionalLockEnabled = YES;
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        
+        _tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+    }
+    return _tableView;
 }
 
 
